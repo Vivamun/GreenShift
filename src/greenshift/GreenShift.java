acd1e5940276e60ed6dd95c043fe191796c2d637
geekman9097 <powerplanets@aol.com>
2017-11-22 07:33:25 -0500

    Launch Code, more bin gitignore struggles
    Actually ran an RM on --cached to make sure.
    
    Side note, I can run git commands from normal system terminal, apparently.

--- src/greenshift/GreenShift.java
+++ src/greenshift/GreenShift.java
@@ -3,8 +3,8 @@ package greenshift;
 public class GreenShift {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("The S.S. Greenshift has been launched.");
+		System.out.println("Launch Simulation Unsucessful. Please implement launch sequence.");
 	}
 
 }
