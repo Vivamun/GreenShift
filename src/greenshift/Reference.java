f61f72fe7b04e8c9a09673ca99b25f63dcdf60aa
geekman9097 <powerplanets@aol.com>
2017-12-03 13:46:23 -0500

    Refererence Updating
    Now More Modular

--- src/greenshift/Reference.java
+++ src/greenshift/Reference.java
@@ -1,17 +1,38 @@
 package greenshift;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Reference {
+	/**
+	 * The title of the game
+	 */
 	public static final String TITLE = "greenshift";
 	/**
 	 * Where to install all files from.
 	 * I.E, where the default files are, and hence, where to download them from.
 	 */
-	public static final URL DATA_URL = null;
+	public static final URL DATA_URL = dataUrl();
+	private static final String DATA_STRING = "";
 	/**
 	 * Where the game is installed, and all files should be located
 	 */
-	public static final File INSTALL_LOCATION = new File(System.getProperty("user.dir")); 
+	public static final File INSTALL_LOCATION = new File(System.getProperty("user.dir"));
+
+	/**
+	 * generate the Data Url Field with try catch blocks
+	 * @return
+	 */
+	private static URL dataUrl() {
+		URL result = null;
+		
+		try {
+			result = new URL(DATA_STRING);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return result;
+	} 
 }
