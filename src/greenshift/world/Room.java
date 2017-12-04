9ac79cbe3430c2daa13e371f52ce3b79cea94856
geekman9097 <powerplanets@aol.com>
2017-12-04 07:42:32 -0500

    Room Boilerplate

--- src/greenshift/world/Room.java
+++ src/greenshift/world/Room.java
@@ -1,5 +1,16 @@
 package greenshift.world;
 
-public class Room {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Room {
+	private final String name;
+	private final String description;
+	private final Map<String, Doorway> doors;
+	
+	public Room(String name, String description) {
+		this.name = name;
+		this.description = description;
+		doors = new HashMap<>();
+	}
 }
