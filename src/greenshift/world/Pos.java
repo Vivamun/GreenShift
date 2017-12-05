db783423db326d85cdaae3f227cd5855375dbc19
geekman9097 <powerplanets@aol.com>
2017-12-05 07:05:46 -0500

    Pos Implementation

--- src/greenshift/world/Pos.java
+++ src/greenshift/world/Pos.java
@@ -2,4 +2,24 @@ package greenshift.world;
 //TODO: Implement Pos class
 public class Pos {
 
+	final int xPos;
+	final int zPos;
+	
+	public Pos(int x, int z) {
+		this.xPos = x;
+		this.zPos = z;
+	}
+	
+	public int getX() {
+		return xPos;
+	}
+	
+	public int getZ() {
+		return zPos;
+	}
+	
+	public Pos move(Direction dir) {
+		return new Pos(xPos + dir.getDelX(), zPos + dir.getDelZ());
+	}
+	
 }
