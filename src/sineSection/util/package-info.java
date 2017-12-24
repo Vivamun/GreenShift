d8a8d0baa421a836723e8cce45148fdb9820cefc
geekman9097 <powerplanets@aol.com>
2017-12-24 11:59:02 -0500

    util package info.
    Starting to document it up in preparation for a possible move to being a seperate entity.

---
+++ src/sineSection/util/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Basic Utility CLasses for use in SineSection Projects.
+ * Classes in this package tend to be static accessible.
+ * These can be referenced by another project, but for code integrity,
+ * should avoid referencing outside the util package.
+ */
+/**
+ * @author geekman9097
+ * @author richie_theRich
+ * @author DMWCincy
+ * @Category Utilities
+ * @version 0.0.5
+ */
+package sineSection.util;
